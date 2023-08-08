6fd04afa86189643f72932923f34a60a802a6fa0
BD-Arfat <arfat1islam5162@gmail.com>
2023-08-08 20:36:26 +0600

    add

--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -5,9 +5,9 @@ import './banner.css'
 const Banner = () => {
 
     return (
-        <div className='mb-48 banner'>
+        <div className='mb-5 banner'>
             
-            <h1 draggable='true' className='text-white md:pt-[300px] md:text-7xl font-bold banner-text1'>Welcome to our website</h1>
+            <h1 draggable='true' className='text-white md:pt-[280px] md:text-7xl font-bold banner-text1'>Welcome to our website</h1>
             <h1 draggable='true' className='mt-8 text-white font-bold text-3xl bannter-text'> ARIFUFL ISLAM ARFAT</h1>
             <h5 className='text-2xl mt-5 text-white font-bold'>I'am A
                 <span className='ms-3 animate' >
