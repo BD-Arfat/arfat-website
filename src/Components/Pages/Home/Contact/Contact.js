efb924fbefa7373fd017e9958ebe0b48906a2a68
BD-Arfat <arfat1islam5162@gmail.com>
2023-09-08 10:41:56 +0600

    add

--- src/Components/Pages/Home/Contact/Contact.js
+++ src/Components/Pages/Home/Contact/Contact.js
@@ -3,48 +3,32 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className='skills mt-10'>
-            <div>
-
-                <div class="container mx-auto my-20 w-1/3 border rounded-lg border-purple-500 bg-info">
-                    <div class="p-5 space-y-5 shadow-xl">
-                        <h4 class="text-center text-white font-bold text-3xl">Contact Us</h4>
-
-                        <form>
-                            <div class="grid grid-cols-2 gap-5">
-                                <input
-                                    type="text"
-                                    class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500"
-                                    placeholder="First Name"
-                                />
-                                <input
-                                    type="text"
-                                    class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500"
-                                    placeholder="Last Name"
-                                />
-                                <input
-                                    type="email"
-                                    class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-                                    placeholder="Email"
-                                />
-                                <input
-                                    type="tel"
-                                    class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-                                    placeholder="Phone"
-                                />
-                                <textarea
-                                    cols="10"
-                                    rows="5"
-                                    class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-                                    placeholder="Write your message..."
-                                ></textarea>
-                            </div>
+            <div class="container mx-auto p-2">
+                <div class="md:w-96 mx-auto my-24 bg-sky-800 px-5 py-10 rounded shadow-xl">
+                    <div class=" mb-8">
+                        <h1 class="font-bold text-3xl">Contact Me</h1>
+                    </div>
+                    <form action="#">
+                        <div class="mt-5">
+                            <label className="label">
+                                <span className="label-text">Your Email</span>
+                            </label>
+                            <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        </div>
+                        <div class="mt-5">
+                            <label className="label">
+                                <span className="label-text">your message</span>
+                            </label>
+                            <textarea className="textarea textarea-bordered w-full h-28" placeholder="You write your words here"></textarea>
+                        </div>
+                        <div class="mt-5">
                             <input
                                 type="submit"
-                                value="Send Message"
-                                class="focus:outline-none mt-5 bg-white px-4 py-2 text-black font-bold w-full"
+                                value="Submit"
+                                class="py-3 bg-green-500 hover:bg-green-600 rounded text-white text-center w-full"
                             />
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- src/Components/Pages/Home/Projects/Project.js
+++ src/Components/Pages/Home/Projects/Project.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Project = ({data}) => {
     return (
         <div>
-            <div className="card md:w-96 bg-info">
+            <div className="card md:w-96 mx-auto bg-info">
                 <figure><img src={data.image} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className=" text-2xl font-bold text-white text-center">{data.name}</h2>
