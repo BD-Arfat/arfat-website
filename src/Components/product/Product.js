33827d501f0394c6a7aaefb7a16eb639bad68ee4
BD-Arfat <arfat1islam5162@gmail.com>
2023-09-12 20:27:50 +0600

    add

--- src/Components/product/Product.js
+++ src/Components/product/Product.js
@@ -10,11 +10,11 @@ const Product = () => {
     
     return (
         <div className='project'>
-            <div className="card lg:card-side bg-base-100 mx-28 shadow-xl my-36">
-                <figure><img src={image} className='w-96 rounded-md ps-3' alt="Album" /></figure>
-                <div className="text-justify w-1/2 ms-5 py-3">
+            <div className="card lg:card-side bg-base-100 mx-5 md:mx-28 shadow-xl my-40 md:my-52">
+                <figure><img src={image} className='md:w-96 rounded-md ps-3' alt="Album" /></figure>
+                <div className="text-justify md:w-1/2 ms-5 py-3">
                     <h2 className="card-title text-3xl font-bold text-white">{name}</h2>
-                    <div className="py-4">
+                    <div className="py-4 px-4">
                     <p>{dis1}</p>
                     <p>{dis2}</p>
                     <p>{dis3}</p>
@@ -29,9 +29,9 @@ const Product = () => {
                         {/* <button  className='btn btn-success me-3'>Live Site Link</button>
                         <button className='btn btn-info me-3'>Client Site Link</button>
                         <button className='btn btn-success me-3'>Server Site Link</button> */}
-                        <a  className='btn btn-success me-3' target='_blank' href={live}>Live Site Link</a>
-                        <a className='btn btn-info me-3' target='_blank' href={github}>Client Site Link</a>
-                        <a  className='btn btn-success me-3' target='_blank' href={server}>Server Site Link</a>
+                        <a  className='btn btn-success text-sm me-3' target='_blank' href={live}>Live Site </a>
+                        <a className='btn btn-info text-sm me-3' target='_blank' href={github}>Client Site </a>
+                        <a  className='btn btn-success text-sm me-3' target='_blank' href={server}>Server Site </a>
                     </div>
                 </div>
             </div>
