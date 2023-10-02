5b1d8e590de317f1104e2301d4b9b51505857033
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-02 12:21:27 +0600

    add

--- src/Components/Pages/Home/Projects/Projects.js
+++ src/Components/Pages/Home/Projects/Projects.js
@@ -11,10 +11,25 @@ const Projects = () => {
         .then(data => setProjects(data))
     } ,[])
 
+    const filterItems =(categItems)=>{
+        const updatedItems = projects.filter((curentElement)=>{
+            return curentElement.filter === categItems;
+
+        });
+        setProjects(updatedItems)
+    }
+
     return (
         <div className='projects mt-5'>
             <div className="">
                 <h1 className='logo font-bold md:text-6xl text-5xl pt-20 pb-16'>My Projects</h1>
+                <div>
+                    <button  className='btn mx-6 btn-outline btn-info' onClick={()=>filterItems('html')}>HTML/CSS</button>
+                    <button  className='btn mx-6 btn-outline btn-info' onClick={()=>filterItems('bootstrap')}>BOOTSTRAPS</button>
+                    <button className='btn mx-6 btn-outline btn-info' onClick={()=>filterItems('tailwind')}>TAILWIND CSS</button>
+                    <button  className='btn mx-6 btn-outline btn-info' onClick={()=>filterItems('full stack')}>FULL STACK</button>
+                    {/* <button  className='btn mx-6 btn-outline btn-success' onClick={()=>setProjects(projects)}>ALL PROJECTS</button> */}
+                </div>
                 <div className='my-20 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                     {
                         projects.map(data => <Project key={data._id} data={data} />)
