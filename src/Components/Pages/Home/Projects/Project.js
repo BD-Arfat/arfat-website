28a5744ddad509b7afee03873ba75b39b818b1fe
BD-Arfat <arfat1islam5162@gmail.com>
2023-10-10 00:09:28 +0600

    add

--- src/Components/Pages/Home/Projects/Project.js
+++ src/Components/Pages/Home/Projects/Project.js
@@ -7,7 +7,7 @@ const Project = ({data}) => {
             <div className="card md:w-96 mx-auto bg-info">
                 <figure><img src={data.image} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className=" text-2xl font-bold text-white text-center">{data.name}</h2>
+                    <h2 className=" text-2xl font-bold text-white text-center">Project <sup>{data.filter}</sup></h2>
                     <p></p>
                     <div className="card-actions justify-end">
                         <Link to={`/product/${data._id}`} className="btn btn-primary btn-outline w-full">All details</Link>
